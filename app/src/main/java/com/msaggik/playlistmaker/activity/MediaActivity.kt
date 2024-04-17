b207e65fd100eed2cfdf296336f78202e77cb238
MSagGik <smw.pantheon@bk.ru>
2024-04-17 14:50:28 +0300

    Создание списка треков (спринт №10)

--- app/src/main/java/com/msaggik/playlistmaker/activity/MediaActivity.kt
+++ app/src/main/java/com/msaggik/playlistmaker/activity/MediaActivity.kt
@@ -1,10 +1,8 @@
-package com.msaggik.playlistmaker
+package com.msaggik.playlistmaker.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.msaggik.playlistmaker.R
 
 class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/msaggik/playlistmaker/activity/SearchActivity.kt
+++ app/src/main/java/com/msaggik/playlistmaker/activity/SearchActivity.kt
@@ -1,22 +1,20 @@
-package com.msaggik.playlistmaker
+package com.msaggik.playlistmaker.activity
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.msaggik.playlistmaker.R
+import com.msaggik.playlistmaker.entity.Track
+import com.msaggik.playlistmaker.util.TrackListAdapter
 
 class SearchActivity : AppCompatActivity() {
     private var textSearch = ""
@@ -27,6 +25,17 @@ class SearchActivity : AppCompatActivity() {
         val inputSearch = findViewById<EditText>(R.id.input_search)
         val buttonBack = findViewById<ImageView>(R.id.button_back)
         val buttonClear = findViewById<ImageView>(R.id.button_clear)
+        val trackListView = findViewById<RecyclerView>(R.id.track_list)
+
+        trackListView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        // инициализация списка треков
+        var trackList: MutableList<Track> = mutableListOf()
+        trackList = mockPlayList(trackList)
+
+        val trackListAdapter = TrackListAdapter(trackList)
+        trackListView.adapter = trackListAdapter
+
 
         inputSearch.setText(textSearch)
 
@@ -84,5 +93,15 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val KEY_TEXT_SEARCH = "KEY_SEARCH"
         const val TEXT_SEARCH_DEFAULT = ""
+    }
+
+    private fun mockPlayList(list: MutableList<Track>): MutableList<Track> {
+        list.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
+        list.add(Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
+        list.add(Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
+        list.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
+        list.add(Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
+        list.add(Track("Sweet Child O'Mine Sweet Child O'Mine Sweet Child O'Mine", "Guns N' Roses Sweet Child O'Mine Sweet Child O'Mine Sweet Child O'Mine", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
+        return list
     }
 }
