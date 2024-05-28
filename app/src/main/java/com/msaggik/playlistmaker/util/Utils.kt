85bbce07f325dbea3a6833528375131dd84045b5
MSagGik <smw.pantheon@bk.ru>
2024-05-28 21:44:27 +0300

    Finalize details

--- app/src/main/java/com/msaggik/playlistmaker/util/Utils.kt
+++ app/src/main/java/com/msaggik/playlistmaker/util/Utils.kt
@@ -3,7 +3,7 @@ package com.msaggik.playlistmaker.util
 import android.content.Context
 import android.util.TypedValue
 
-object Util {
+object Utils {
     fun doToPx(dp: Float, context: Context): Int {
         return TypedValue
             .applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
