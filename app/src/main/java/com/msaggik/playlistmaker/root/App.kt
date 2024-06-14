1d7ce6eb095aba90ba1995b9994454898660e68d
MSagGik <smw.pantheon@bk.ru>
2024-06-14 12:53:50 +0300

    Refactor DI Koin (modules Setting and Sharing)

--- app/src/main/java/com/msaggik/playlistmaker/root/App.kt
+++ app/src/main/java/com/msaggik/playlistmaker/root/App.kt
@@ -3,53 +3,36 @@ package com.msaggik.playlistmaker.root
 import android.app.Application
 import com.msaggik.playlistmaker.player.di.playerModule
 import com.msaggik.playlistmaker.search.di.searchModule
-import com.msaggik.playlistmaker.setting.data.base.impl.ThemeSpImpl
-import com.msaggik.playlistmaker.setting.domain.repository.SettingRepository
-import com.msaggik.playlistmaker.setting.data.repository_impl.SettingRepositoryImpl
+import com.msaggik.playlistmaker.setting.di.settingModule
 import com.msaggik.playlistmaker.setting.domain.api.SettingsInteractor
-import com.msaggik.playlistmaker.setting.domain.api.impl.SettingsInteractorImpl
 import com.msaggik.playlistmaker.setting.domain.model.ThemeSettings
-import com.msaggik.playlistmaker.sharing.domain.repository.ExternalNavigator
-import com.msaggik.playlistmaker.sharing.data.ExternalNavigatorImpl
-import com.msaggik.playlistmaker.sharing.domain.api.SharingInteractor
-import com.msaggik.playlistmaker.sharing.domain.api.impl.SharingInteractorImpl
+import com.msaggik.playlistmaker.sharing.di.sharingModule
 import com.msaggik.playlistmaker.util.Utils
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 
 
-class App : Application() {
+class App : Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
-
-        provideThemeInteractor().getThemeSettingsConsumer(object : SettingsInteractor.ThemeConsumer {
-            override fun consume(themeSettings: ThemeSettings) {
-                Utils.setApplicationTheme(themeSettings.isDarkTheme)
-            }
-        })
 
         startKoin{
             androidContext(this@App)
             modules(
                 playerModule,
                 searchModule,
+                settingModule,
+                sharingModule,
             )
         }
-    }
 
-    fun provideThemeInteractor(): SettingsInteractor {
-        return SettingsInteractorImpl(getThemeRepository())
-    }
-
-    fun provideSharingInteractor(): SharingInteractor {
-        return SharingInteractorImpl(getSharingRepository())
-    }
-
-    private fun getThemeRepository(): SettingRepository {
-        return SettingRepositoryImpl(ThemeSpImpl(applicationContext))
-    }
-
-    private fun getSharingRepository(): ExternalNavigator {
-        return ExternalNavigatorImpl(applicationContext)
+        val provideThemeInteractor: SettingsInteractor by inject()
+        provideThemeInteractor.getThemeSettingsConsumer(object : SettingsInteractor.ThemeConsumer {
+            override fun consume(themeSettings: ThemeSettings) {
+                Utils.setApplicationTheme(themeSettings.isDarkTheme)
+            }
+        })
     }
 }
--- app/src/main/java/com/msaggik/playlistmaker/setting/data/base/ThemeSp.kt
+++ app/src/main/java/com/msaggik/playlistmaker/setting/data/base/ThemeSp.kt
@@ -1,17 +1,6 @@
 package com.msaggik.playlistmaker.setting.data.base
 
-import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
-
-private const val SHARE_PREFERENCES_THEME = "share_preferences_theme"
 interface ThemeSp {
-    companion object{
-        fun createObjectSpTheme(context: Context): SharedPreferences {
-            return context.getSharedPreferences(SHARE_PREFERENCES_THEME, Application.MODE_PRIVATE)
-        }
-    }
-
     fun isDarkThemeSharedPreferences() : Boolean
     fun updateThemeSettingSharedPreferences(isDarkTheme : Boolean)
 }
